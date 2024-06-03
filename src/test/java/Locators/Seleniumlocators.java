d5e5b4460d08e87fe36f8855156726f5c82d4541
mohan Nikshith <mohannayak.56@gmail.com>
2024-06-03 14:29:49 +0530

    infoedit codes

--- src/test/java/Locators/Seleniumlocators.java
+++ src/test/java/Locators/Seleniumlocators.java
@@ -1,11 +1,14 @@
 package Locators;
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 public class Seleniumlocators {
 
 	public static void main(String[] args) 
 	
 	{
-		WebDriver driver=new Chromedriver();
+		 WebDriver driver = new ChromeDriver();
+		 driver.get("https://www.google.com");
+		 System.out.println("I am old");
 
 	}
 
